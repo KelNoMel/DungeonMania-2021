c490e8f03384c6ba1ccdd0bd00ea136695326a49
Matthew Brian <matt@brians.com.au>
2021-10-14 14:23:48 +1100

    DungeonManiaController DocumentaA RuntimeException to throw when an invalid action is attempted (See Section 7.2).

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Contains one method for each command you need to implement.
+ */
 public class DungeonManiaController {
     public DungeonManiaController() {
     }
@@ -38,32 +41,85 @@ public class DungeonManiaController {
             return new ArrayList<>();
         }
     }
-
+    
+    /**
+     * Creates a new game, where dungeonName is the name of the dungeon map (corresponding to
+     * a JSON file stored in the model) and gameMode is one of "standard", "peaceful" or "hard".
+     * @param dungeonName
+     * @param gameMode
+     * @return
+     * @throws IllegalArgumentException If gameMode is not a valid game mode
+     * @throws IllegalArgumentException If dungeonName is not a dungeon that exists
+     */
     public DungeonResponse newGame(String dungeonName, String gameMode) throws IllegalArgumentException {
         return null;
     }
     
+    /**
+     * Saves the current game state with the given ID.
+     * @param name
+     * @return
+     * @throws IllegalArgumentException If id is not a valid game id
+     */
     public DungeonResponse saveGame(String name) throws IllegalArgumentException {
         return null;
     }
 
+    /**
+     * Loads the game with the given id.
+     * @param name
+     * @return
+     * @throws IllegalArgumentException If id is not a valid game id
+     */
     public DungeonResponse loadGame(String name) throws IllegalArgumentException {
         return null;
     }
 
+    /**
+     * Returns a list containing all the saved games that are currently stored
+     * @return
+     */
     public List<String> allGames() {
         return new ArrayList<>();
     }
 
+    /**
+     * Ticks the game state. When a tick occurs:
+     * 1. The player moves in the specified direction one square
+     * 2. All enemies move respectively
+     * 3. Any items which are used are 'actioned' and interact with the relevant entity
+     * @param itemUsed
+     * @param movementDirection
+     * @return
+     * @throws IllegalArgumentException If itemUsed is not one of bomb, invincibility_potion, invisibility_potion
+     * @throws InvalidActionException If itemUsed is not in the player's inventory
+     */
     public DungeonResponse tick(String itemUsed, Direction movementDirection) throws IllegalArgumentException, InvalidActionException {
         return null;
     }
-
-    public DungeonResponse interact(String entityId) throws IllegalArgumentException, InvalidActionException {
+    
+    /**
+     * Builds the given entity, where buildable is one of bow and shield.
+     * @param entityId
+     * @return
+     * @throws IllegalArgumentException If buildable is not one of bow, shield
+     * @throws InvalidActionException If the player does not have sufficient items to craft the buildable
+     */
+    public DungeonResponse build(String buildable) throws IllegalArgumentException, InvalidActionException {
         return null;
     }
 
-    public DungeonResponse build(String buildable) throws IllegalArgumentException, InvalidActionException {
+    /**
+     * Interacts with a mercenary (where the character bribes the mercenary) or a zombie spawner, where
+     * the character destroys the spawner.
+     * @param buildable
+     * @return
+     * @throws IllegalArgumentException If entityId is not a valid entity ID
+     * @throws InvalidActionException If the player is not cardinally adjacent to the given entity
+     * @throws InvalidActionException If the player does not have any gold and attempts to bribe a mercenary
+     * @throws InvalidActionException If the player does not have a weapon and attempts to destroy a spawner
+     */
+    public DungeonResponse interact(String entityId) throws IllegalArgumentException, InvalidActionException {
         return null;
     }
 }
