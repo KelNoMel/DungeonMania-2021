e8f029f5f7d6cd8f6da1fe613e12deb7b4306d7a
Hayley Yu <z5218663@ad.unsw.edu.au>
2021-10-24 13:38:28 +1100

    initial goal & goal condition implementation

--- src/main/java/dungeonmania/Dungeon.java
+++ src/main/java/dungeonmania/Dungeon.java
@@ -62,6 +62,7 @@ public class Dungeon {
     	
     	dungeonId = Dungeon.createId();
     	// this.goalCondition = 
+        // GoalCondition.AND // GoalCondition.OR
     }
     
     public Dungeon(File loadFile) {
