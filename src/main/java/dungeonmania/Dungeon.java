5827315750a7900be70082fe482fc9ec9dd3e8fb
Hayley Yu <z5218663@ad.unsw.edu.au>
2021-10-27 12:32:53 +1100

    initial exit implementation

--- src/main/java/dungeonmania/Dungeon.java
+++ src/main/java/dungeonmania/Dungeon.java
@@ -215,6 +215,10 @@ public class Dungeon {
     	}
     	return posEnts;
     }
+
+	public Entity getPlayer() {
+		return player;
+	}
     
 	// TODO: add goals, buildables, animations
     /**
---
+++ src/test/java/dungeonmania/ExitTest.java
@@ -0,0 +1,102 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import java.lang.IllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.testhelper.ResponseHelp;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class ExitTest {
+
+    @Test
+    public void testPlayerAtExit() {
+        DungeonManiaController mania = new DungeonManiaController();
+        mania.newGame("maze", "Peaceful");
+
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.RIGHT);
+
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+        mania.tick(null, Direction.UP);
+
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+        mania.tick(null, Direction.RIGHT);
+
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+        mania.tick(null, Direction.DOWN);
+
+        DungeonResponse movedResponse = mania.tick(null, Direction.DOWN);
+
+        // TODO : 
+        // Check that the player moved 
+        //assertTrue(ResponseHelp.entityInDungeon(new EntityResponse("", "player", new Position(1, 1), false), movedResponse));
+
+        // Check that player is at exit
+        //assertTrue(ResponseHelp.entityInDungeon(new EntityResponse("", "player", new Position(18, 16), false), movedResponse));
+        //assertTrue(ResponseHelp.entityInDungeon(new EntityResponse("", "exit", new Position(18, 16), false), movedResponse));
+
+
+        // check that Exit.playerAtExit() returns true // TODO: check via goal condition being met. 
+
+    }
+    
+}
