c354c73e605a7d4bb727b10482ae623baa5bcfc7
Matthew Brian <matt@brians.com.au>
2021-10-27 16:22:23 +1100

    Added Portal Functionality and Assumptions
    Portals currently teleport all entities on top of themto their other portal linked by colour. Other assumptions made while programming this are in assumptions.md

---
+++ assumptions.md
@@ -0,0 +1,5 @@
+
+Portals
+- Portals teleport entities directly onto their corresponding portal
+- Only two portals of a specific colour can exist at any time (as in the Portal games)
+- Portals read in as JSON will always come in pairs, solo portals will not be fed in
--- src/main/java/dungeonmania/entities/statics/Portal.java
+++ src/main/java/dungeonmania/entities/statics/Portal.java
@@ -1,5 +1,10 @@
 package dungeonmania.entities.statics;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import dungeonmania.Dungeon;
 import dungeonmania.InputState;
 import dungeonmania.entities.Entity;
@@ -7,16 +12,56 @@ import dungeonmania.util.Position;
 
 public class Portal extends Entity {
 
-	public Portal(Dungeon dungeon, Position position) {
-		super(dungeon, "Portal", position, true);
+	private static Map<String, PortalLink> portalLinks = new HashMap<>();
+	private class PortalLink {
+		private Portal p1;
+		private Portal p2;
+		
+		private PortalLink(Portal p1, Portal p2) {
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+	}
+	
+	private String colour;
+	// Entities teleported already this tick
+	private List<Entity> teleportedEntities = new ArrayList<>();
+	
+	public Portal(Dungeon dungeon, Position position, String colour) {
+		super(dungeon, "portal-" + colour, position, false);
+		this.colour = colour;
+
+		// Create a new portal link, and link this to the other portal of the same colour if link already exists
+		if (portalLinks.putIfAbsent(colour, new PortalLink(this, null)) != null) {
+			portalLinks.get(colour).p2 = this;
+		}
 	}
 
 	protected void inputEntity(InputState inputState) {
-
+		
 	}
 
 	protected void updateEntity() {
+		List<Entity> entitiesOnThisPortal = dungeon.getEntitiesAtPosition(getPosition());
 
+		entitiesOnThisPortal.remove(this);
+		// Only teleport entities that haven't already been teleported this tick
+		entitiesOnThisPortal.removeAll(teleportedEntities);
+		
+		Portal teleportPortal = getCorrespondingPortal();
+		for (Entity e : entitiesOnThisPortal) {
+			// Teleport!
+			e.setPosition(teleportPortal.getPosition());
+			teleportPortal.teleportedEntities.add(e);
+		}
+		teleportedEntities.clear();
 	}
-
+	
+	private Portal getCorrespondingPortal() {
+		if (getId().equals(portalLinks.get(colour).p1.getId())) {
+			return portalLinks.get(colour).p2;
+		} else {
+			return portalLinks.get(colour).p1;
+		}
+	}
 }
