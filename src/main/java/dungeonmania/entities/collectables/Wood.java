0ac39ae8bb8bd02be1057d3f21ec38782ee9b946
Hayley Yu <z5218663@ad.unsw.edu.au>
2021-10-30 19:23:10 +1100

    wood

--- src/main/java/dungeonmania/entities/collectables/Wood.java
+++ src/main/java/dungeonmania/entities/collectables/Wood.java
@@ -3,10 +3,13 @@ package dungeonmania.entities.collectables;
 import dungeonmania.Dungeon;
 import dungeonmania.InputState;
 import dungeonmania.components.AIComponent;
+import dungeonmania.components.CollectableComponent;
+import dungeonmania.components.CollectableState;
 import dungeonmania.entities.Entity;
 import dungeonmania.util.Position;
 
 public class Wood extends Entity {
+	private CollectableComponent collectableComp = new CollectableComponent(this, 1, CollectableState.MAP);
 
 	public Wood(Dungeon dungeon, Position position) {
 		super(dungeon, "wood", position, false);
