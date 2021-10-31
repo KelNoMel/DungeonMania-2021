def2149750ce155c3037d0ddd2cbd2ac267f8e99
KelNoMel <68493100+KelNoMel@users.noreply.github.com>
2021-10-31 21:50:51 +1100

    Starting bomb

--- src/main/java/dungeonmania/entities/collectables/Bomb.java
+++ src/main/java/dungeonmania/entities/collectables/Bomb.java
@@ -9,12 +9,22 @@ import dungeonmania.util.Position;
 
 public class Bomb extends Entity {
 
+
+	private CollectableComponent collectableComp = new CollectableComponent(this, 1, CollectableState.MAP);
+	private ConsumableComponent consumableComp = new ConsumableComponent(this, 2, 1, 1);
+
 	public Bomb(Dungeon dungeon, Position position, JSONObject entitySpecificData) {
 		super(dungeon, "bomb", position, false, entitySpecificData);
 	}
 
 	protected void inputEntity(InputState inputState) {
-
+		// Check if item was queued to be used
+		if (getDungeon().getPlayer().getUsedList().containsKey(getId())) {
+			
+			// Effects of bomb: Bomb is placed on the current tile the player is on
+			getDungeon().getPlayer().addComponent(EffectComponent(EffectComponent(getDungeon().getPlayer(), 3));
+			getDungeon().getPlayer().setStatus("invincible");
+		}
 	}
 
 	protected void updateEntity() {
