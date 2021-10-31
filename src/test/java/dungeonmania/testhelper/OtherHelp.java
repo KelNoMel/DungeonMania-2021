749d076d08133f1ca03155a70a49be7760c84198
Matthew Brian <matt@brians.com.au>
2021-11-01 09:30:29 +1100

    MajorFix: Fixed Saving Bug
    Fixed bug with file saving that broke tests

--- src/test/java/dungeonmania/testhelper/OtherHelp.java
+++ src/test/java/dungeonmania/testhelper/OtherHelp.java
@@ -2,16 +2,13 @@ package dungeonmania.testhelper;
 
 import java.io.File;
 import java.net.URISyntaxException;
-import java.nio.file.Paths;
 
 import dungeonmania.util.FileLoader;
 
 public class OtherHelp {
 	public static void removeSaves() {
 		try {
-			String dungeonFolder = FileLoader.getFolderPath("/dungeons").toString();
-			dungeonFolder = dungeonFolder.substring(0, dungeonFolder.lastIndexOf("\\"));
-			File saveDir = Paths.get(dungeonFolder, "dungeonSaves").toFile();
+			File saveDir = FileLoader.getSavePath().toFile();
 			for (File f : saveDir.listFiles()) {
 				if (!f.isDirectory()) {
 					f.delete();
