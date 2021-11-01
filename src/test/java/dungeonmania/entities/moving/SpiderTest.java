e4780ef62230879f7b82e6910927265ac607281d
Matthew Johnstone <z5255782@ad.unsw.edu.au>
2021-11-01 11:38:25 +1100

    spider tests added

--- src/test/java/dungeonmania/entities/moving/SpiderTest.java
+++ src/test/java/dungeonmania/entities/moving/SpiderTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.testhelper.ResponseHelp;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
 
 public class SpiderTest {
 	@Test
-	public void testStub() {
+	public void testSpider() {
 		DungeonManiaController mania = new DungeonManiaController();
-        mania.newGame("maze","Peaceful");
+        DungeonResponse res = mania.newGame("spider","Hard");
+		assertTrue(ResponseHelp.getEntityOfType(res, "spider").getPosition().equals(new Position(0, 2)));
+		mania.tick(null, Direction.DOWN);
+		res = mania.tick(null, Direction.UP);
+		// spider should is likely dead
+		assertTrue(ResponseHelp.getEntityOfType(res, "spider") == null);
 	}
 }
