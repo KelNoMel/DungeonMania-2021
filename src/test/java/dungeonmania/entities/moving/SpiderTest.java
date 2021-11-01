7effc5f3630e6b57c252875c40c9b2120a7784e8
Matthew Johnstone <z5255782@ad.unsw.edu.au>
2021-11-01 11:43:43 +1100

    more spider tests added

--- src/test/java/dungeonmania/entities/moving/SpiderTest.java
+++ src/test/java/dungeonmania/entities/moving/SpiderTest.java
@@ -20,4 +20,26 @@ public class SpiderTest {
 		// spider should is likely dead
 		assertTrue(ResponseHelp.getEntityOfType(res, "spider") == null);
 	}
+
+	@Test
+	public void testSpiderSpawning() {
+		DungeonManiaController mania = new DungeonManiaController();
+        DungeonResponse res = mania.newGame("spider","Hard");
+		assertTrue(ResponseHelp.getEntityOfType(res, "spider").getPosition().equals(new Position(0, 2)));
+		mania.tick(null, Direction.DOWN);
+		res = mania.tick(null, Direction.UP);
+		// spider is likely dead
+		assertTrue(ResponseHelp.getEntityOfType(res, "spider") == null);
+
+		for (int i = 0; i < 100; i++) {
+			res = mania.tick(null, Direction.UP);
+			res = mania.tick(null, Direction.DOWN);
+			res = mania.tick(null, Direction.LEFT);
+			res = mania.tick(null, Direction.RIGHT);
+		}
+
+		// new spiders have spawned
+		assertTrue(ResponseHelp.getEntityOfType(res, "spider") != null);
+
+	}
 }
