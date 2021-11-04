04d26f71a683d028799601e8d5a9bd609a7129cf
z5308709 <z5308709@cse.unsw.edu.au>
2021-11-05 07:23:09 +1100

    sun stone basic constructors added

--- src/main/java/dungeonmania/entities/collectables/Sun_stone.java
+++ src/main/java/dungeonmania/entities/collectables/Sun_stone.java
@@ -1,5 +1,43 @@
 package dungeonmania.entities.collectables;
 
-public class Sun_stone {
-    
+import org.json.JSONObject;
+
+import dungeonmania.Dungeon;
+import dungeonmania.InputState;
+import dungeonmania.components.CollectableComponent;
+import dungeonmania.components.CollectableState;
+import dungeonmania.entities.Entity;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Position;
+
+public class Sun_stone extends Entity {
+    private CollectableComponent collectableComp = new CollectableComponent(this, 1, CollectableState.MAP);
+
+    public Sun_stone(Dungeon dungeon, Position position, JSONObject entitySpecificData) {
+		super(dungeon, "sun_stone", position, false, entitySpecificData);
+	}
+
+    @Override
+    protected void loadJSONEntitySpecific(JSONObject entitySpecificData) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void inputEntity(InputState inputState) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void updateEntity() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addJSONEntitySpecific(JSONObject baseJSON) {
+        // TODO Auto-generated method stub
+        
+    }
 }
