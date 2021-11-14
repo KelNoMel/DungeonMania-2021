a71411eae0ac1bb19bd06b01ad71c5ca5c14a84f
z5308709 <z5308709@cse.unsw.edu.au>
2021-11-14 19:41:54 +1100

    midnight armour

--- src/main/java/dungeonmania/entities/buildable/MidnightArmour.java
+++ src/main/java/dungeonmania/entities/buildable/MidnightArmour.java
@@ -6,7 +6,10 @@ import dungeonmania.Dungeon;
 import dungeonmania.InputState;
 import dungeonmania.util.Position;
 import dungeonmania.components.CollectableComponent;
+import dungeonmania.entities.Entity;
+import dungeonmania.entities.moving.ZombieToast;
 import dungeonmania.components.ArmourComponent;
+import dungeonmania.components.BattleComponent;
 import dungeonmania.components.BattleItemComponent;
 
 public class MidnightArmour extends Buildable {
@@ -16,15 +19,25 @@ public class MidnightArmour extends Buildable {
 	public CollectableComponent collectableComponent;
 	public BattleItemComponent battleItemComponent;
 	public ArmourComponent armourComponent;
+	public BattleComponent battleComponent;
 
 	public MidnightArmour(Dungeon dungeon, Position position, JSONObject entitySpecificData)  {
 		super(dungeon,  BuildableEnum.MIDNIGHT_ARMOUR.getType(), position, false, BuildableEnum.MIDNIGHT_ARMOUR.getRecipe(), entitySpecificData);
 		battleItemComponent = new BattleItemComponent(this, 1, totalDurability);
 		armourComponent = new ArmourComponent(this, 2, armour);
+		battleComponent = new BattleComponent(this, 3, 30, 10);
 	}
 
-	protected void inputEntity(InputState inputState) {}
-	protected void updateEntity() {}
+	protected void inputEntity(InputState inputState) {
+		Dungeon d = getDungeon();
+		for (Entity e : d.getEntities()) {
+			if (e instanceof ZombieToast) { return; }
+		}
+	}
+
+	protected void updateEntity() {
+		battleComponent.setAttackDamage(15);
+	}
 
 	public void addJSONEntitySpecific(JSONObject baseJSON) {}
 	protected void loadJSONEntitySpecific(JSONObject entitySpecificData) {}
